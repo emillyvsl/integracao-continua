c484eea5924a295998d57dc5e1573709745a0d1d
Emilly Silva <115952368+emillyvsl@users.noreply.github.com>
2023-06-16 20:24:23 -0500

    Update FiltroCors.java

--- sgcmapi/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
+++ sgcmapi/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
@@ -16,8 +16,10 @@ public class FiltroCors {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowCredentials(true);
         corsConfig.setAllowedOrigins(Arrays.asList(
+            "https://sgcmapp-emi.up.railway.app",
             "http://127.0.0.1:5500",
             "https://localhost:4200"
+            
         ));
         corsConfig.setAllowedMethods(Arrays.asList("*"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
